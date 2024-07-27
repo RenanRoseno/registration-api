44c9aee3db9b1f504f1be3a8c08a00cd620de249
Renan Roseno <rosenorenan@gmail.com>
2024-07-27 14:14:21 -0300

    fix: adapting eureka configurations

--- src/main/java/com/queonetics/registration/RegistrationApplication.java
+++ src/main/java/com/queonetics/registration/RegistrationApplication.java
@@ -2,8 +2,10 @@ package com.queonetics.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
